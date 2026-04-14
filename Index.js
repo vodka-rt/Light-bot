195b4a528e1861cc4958eccd72129bf5ef77fb35
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-04-14 16:08:34 -0300

    Implement permission check for '!say' command
    Added permission check for the '!say' command and improved message handling.

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-require("./server");
 
 const client = new Client({
   intents: [
@@ -14,5 +13,22 @@ const prefix = "!";
 client.on('messageCreate', async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // só ADM pode usar
-  if (!
+  if (!message.member.permissions.has("Administrator")) return;
+
+  const args = message.content.split(" ");
+
+  if (args[0] === "!say") {
+    const canal = message.mentions.channels.first();
+    if (!canal) return message.reply("Marca um canal!");
+
+    const texto = args.slice(2).join(" ");
+
+    const embed = new EmbedBuilder()
+      .setDescription(texto)
+      .setColor("#313338");
+
+    canal.send({ embeds: [embed] });
+  }
+});
+
+client.login(process.env.TOKEN);
