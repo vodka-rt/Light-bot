5c1fabf073a82cf092ac2ecc4b86d9199db08e3f
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-04-14 23:19:54 -0300

    Update giveaway.js

--- models/giveaway.js
+++ models/giveaway.js
@@ -1,1 +1,12 @@
+const mongoose = require("../database");
 
+const giveawaySchema = new mongoose.Schema({
+  messageId: String,
+  channelId: String,
+  prize: String,
+  winners: Number,
+  endAt: Number,
+  users: [String]
+});
+
+module.exports = mongoose.model("Giveaway", giveawaySchema);
