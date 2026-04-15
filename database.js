ba5d86414867fa1bcf8be3d6706d908b2d41e197
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-04-14 23:51:23 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("🟢 Banco conectado");
-  } catch (err) {
-    console.log("🔴 Erro MongoDB:", err);
-  }
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("🟢 Banco conectado");
 }
 
 module.exports = connectDB;
