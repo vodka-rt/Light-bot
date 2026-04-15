098b5918c8e2a55f8fb4e876994dae0d8301eda9
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-04-14 23:43:02 -0300

    Update guild.js

--- models/guild.js
+++ models/guild.js
@@ -1,15 +1,9 @@
-const mongoose = require("../database");
+const mongoose = require("mongoose");
 
 const guildSchema = new mongoose.Schema({
   guildId: String,
-
-  // canal de logs
   logChannel: String,
-
-  // anti spam
   antiSpam: { type: Boolean, default: false },
-
-  // anti raid
   antiRaid: { type: Boolean, default: false }
 });
 
