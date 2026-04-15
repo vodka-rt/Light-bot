62c3f3b15796bfafcdb4e5f37ccf379f4c7d4d33
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-04-14 23:19:45 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,9 @@
+const mongoose = require("../database");
 
+const userSchema = new mongoose.Schema({
+  userId: String,
+  xp: { type: Number, default: 0 },
+  money: { type: Number, default: 1000 }
+});
+
+module.exports = mongoose.model("User", userSchema);
